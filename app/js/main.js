f161bf2e8fdad7906ab8c9bc941730b770217a49
nobodynoticed <eugenejukov23@gmail.com>
2020-04-22 01:18:51 +0300

    added section slider-followers, section info, section choose

--- app/js/main.js
+++ app/js/main.js
@@ -5,12 +5,30 @@ $(document).ready(function () {
         nextArrow: '<span class="lnr lnr-chevron-right"></span>'
     });
 
+    $('.slider-followers__slider').slick({
+        arrows: true,
+        prevArrow: '<span class="lnr lnr-chevron-left"></span>',
+        nextArrow: '<span class="lnr lnr-chevron-right"></span>',
+        slidesToShow: 3,
+        // centerMode: true,
+        infinite: true
+    });
+
+    
+
     $(".rate-star").rateYo({
         rating: 4.5,
         spacing: "3px",
         starWidth: '17px',
         ratedFill: "#ffc000",
+        readOnly: true
+    });
+
+    $(".prod-star").rateYo({
         rating: 4.5,
+        spacing: '3px',
+        starWidth: '15px',
+        ratedFill: '#ffc000',
         readOnly: true
     });
 
